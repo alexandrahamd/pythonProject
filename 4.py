19b013bc741a54783b16d111890e5c8eab79708e
alexandrahamd <43954475+alexandrahamd@users.noreply.github.com>
2022-12-07 17:47:47 +0300

    Update 4.py
    update 4.py

--- 4.py
+++ 4.py
@@ -3,10 +3,27 @@ class MyInt:
         self.x = x
 
     @classmethod
-    def __check(cls, x):
-        if not isinstance(x, (int, str)):
-            return False
-        return True
+    def __check(cls, other):
+        if not isinstance(other, (str, int, MyInt)):
+            raise TypeError("Операнд справа должен иметь тип int или MyInt")
+
+        return int(other) if isinstance(other, (str, int)) else other.x
+    
+     def __add__(self, other):
+        if self.__check(other):
+            return self.x + int(other)
+
+    def __sub__(self, other):
+        if self.__check(other):
+            return self.x - int(other)
+
+    def __mul__(self, other):
+        if self.__check(other):
+            return self.x * int(other)
+
+    def __truediv__(self, other):
+        if self.__check(other):
+            return self.x / int(other)
 
     def __repr__(self):
         return f"{self.__class__}: {self.x}"
@@ -31,9 +48,9 @@ a = MyInt(5)
 print(a)
 a = a > 5
 print(a)
-a = a <= 5
-print(a)
 a = a < 2
 print(a)
 a = a <= 2
+print(a)
+a = a > "5"
 print(a)
